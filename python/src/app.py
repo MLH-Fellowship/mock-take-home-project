94952b3423b5dcec7998ffbc499fce472bd11859
Ciara (CiCi) Swann <cccswann@gmail.com>
2021-12-04 01:23:25 +0000

    tests PASS

--- python/src/app.py
+++ python/src/app.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, Response, jsonify, request, abort
+import json 
+import re
 
 app = Flask(__name__)
 
+date_regex = re.compile("\d{4}-\d{2}-\d{2}")
+#activities = [{"date": "2021-12-01", "name":"Cycling", "duration": 45, "distance": 10}]
+activities = []
+
+def is_valid(activity: dict) -> bool:
+    date = activity.get("date", "")
+    if re.match(date_regex,date) is None:
+        return False  
+    try:
+        name = activity.get("name")
+        duration = activity.get("duration")
+        distance = activity.get("distance")
+        #jassert isinstance(duration, int)
+        #assert isinstance(distance, int)
+    except:
+        return False
+    return True
+
 @app.route('/')
 def index():
-    return "Welcome to Sports Hub!"
+    return "Welcome to Sports Hub!", 200
 
-@app.route('/activity', ['GET', 'POST'])
+@app.route('/activity', methods=['GET', 'POST'])
 def activity():
-    return "Error!"
+    if request.method == "GET":
+        return jsonify(activities)
+
+    elif request.method == "POST":
+        activity = request.get_json()
+        app.logger.info(activity)
+        if activity is None or not is_valid(activity):
+            abort(400)
+        activities.append(activity)
+        return "Record Added!", 200
+    else:
+        return "Error!", 400
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- python/tests/tests.py
+++ python/tests/tests.py
@@ -9,4 +9,12 @@ def test_response():
     assert [] == requests.get('http://localhost:5000/activity').json()
 
 def test_post():
-    assert "Record Added!" == requests.post('http://localhost:5000/activity', data={"date": "2021-12-01", "name":"Cycling", "duration": 45, "distance": 10}).text
+    assert "Record Added!" == requests.post('http://localhost:5000/activity', json={"date": "2021-12-01", "name":"Cycling", "duration": 45, "distance": 10}).text
+
+# TODO: An API call to any of the command with incomplete value should return 400.
+def test_incomplete():
+    assert 404 == requests.get('http://localhost:5000/activity/fdkjal').status_code
+
+def test_invalid_activity():
+    resp = requests.post('http://localhost:5000/activity', json={"date":"1234", "name":"excited basketweaving"})
+    assert resp.status_code == 400
