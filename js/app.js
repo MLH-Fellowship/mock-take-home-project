b2f9424587085dc1f0cf7ea188e2d07f3b83ad7c
Nandini Bansal <nandinibansal14@gmail.com>
2021-12-06 23:30:46 +0530

    add routes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+const bodyParser = require("body-parser");
 var express = require("express");
+const Sqlite = require("sqlite3");
 var app = express();
 const userRoutes = require("./routes/user");
 
 app.use("/activity/", userRoutes);
 
-exports.modules = app;
+// const db = new Sqlite.Database("database/activity.db");
+// db.run(
+//   "CREATE TABLE IF NOT EXISTS activity(date DATE, name TEXT, time TIME, distance TEXT)"
+// );
+
+app.use(bodyParser.json());
+
+module.exports = app;
--- js/routes/user.js
+++ js/routes/user.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// router.post("/", (res, req) => {
+//   co
+// });
+
+module.exports = router;
