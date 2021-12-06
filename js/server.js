90e4b1e8ae107b4a917db66d6e2807961d9013d4
Hargun Kaur <56452820+hkaur008@users.noreply.github.com>
2021-12-06 18:15:58 +0000

    get api added

--- js/server.js
+++ js/server.js
@@ -4,3 +4,7 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on Port :${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.status(200).send({"message" : "work success"});
+});
