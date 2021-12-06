db05abab7fddaef94f3fcae68cce116ccfcd4847
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-12-05 20:54:23 -0500

    Update app.py

--- python/src/app.py
+++ python/src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +8,58 @@ app = Flask(__name__)
 def index():
     return "Welcome to Sports Hub!"
 
-@app.route('/activity', ['GET', 'POST'])
+def create_table(conn):
+    conn.cursor().execute("""
+        CREATE TABLE IF NOT EXISTS
+        activities(
+            name TEXT,
+            date TEXT,
+            duration INTEGER,
+            distance INTEGER
+        )
+    """)
+
+def connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('activity.db')
+    except Exception as ex:
+        print(ex)
+    return conn
+
+@app.route('/activity', methods=['POST', 'GET'])
 def activity():
-    return "Error!"
+    conn = connection()
+    cursor = conn.cursor()
+    #create table
+    create_table(conn)
+
+    if request.method == 'GET':
+        try:
+            data = cursor.execute("select * from activities")
+            result = cursor.fetchall()
+            toReturn = []
+            for row in result:
+                toReturn.append({
+                    "name":row[0],
+                    "date":row[1],
+                    "duration":row[2],
+                    "distance":row[3]
+                })
+            return jsonify(toReturn),200
+        except Exception as ex:
+            print("woopsies, something went wrong lol")
+            return "error", 400
+    else:
+        try:
+            req = request.get_json()
+            data = (req["name"], req["date"], req["duration"], req["distance"])
+            cursor.execute("INSERT INTO activities VALUES(?,?,?,?)",data)
+            conn.commit()
+        except Exception as ex:
+            print("woopsies, something went wrong lol")
+            return "error", 400
+
+        return "Record Added!", 200
+    
+
