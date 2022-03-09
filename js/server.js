f3168a25c6221ac532e6bbd2183d0420bb64cd25
Arsalan Khattak <me@khattak.dev>
2022-03-09 17:44:36 +0500

    Add js boilerplate

--- js/server.js
+++ js/server.js
@@ -1,6 +1,50 @@
-const app = require("./app");
-
+const express = require("express");
+const app = express();
 const port = 3000;
+
+const activities = [
+  {
+    id: 0,
+    date: "2022-03-04",
+    name: "Running",
+    duration: 60,
+    distance: 4.3,
+  },
+  {
+    id: 1,
+    date: "2022-03-06",
+    name: "Cycling",
+    duration: 120,
+    distance: 25,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Sports Hub!");
+});
+
+app.get("/activities", (req, res) => {
+  res.send("Record Fetched!");
+});
+
+app.post("/activities/new", (req, res) => {
+  // Complete me
+  // Add a new activitiy to the activites array
+  res.send("Add record!");
+});
+
+app.put("/activities/change/:id", (req, res) => {
+  // Complete me
+  // Change the data for an activity inside of the activities array
+  res.send("Activity changed!");
+});
+
+app.put("/activities/delete/:id", (req, res) => {
+  // Complete me
+  // Delete an activity inside of the activities array
+  res.send("Activity deleted!");
+});
+
 app.listen(port, () => {
-    console.log(`Server started on Port :${port}`);
+  console.log(`App listening on port ${port}`);
 });
